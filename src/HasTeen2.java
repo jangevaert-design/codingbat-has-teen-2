73900d4ece637f4691ef416be880f950c46f5ca5
Jan Gevaert <jan.gevaert@gmail.com>
2020-10-18 07:03:51 -0600

    Initial commit

---
+++ src/HasTeen2.java
@@ -0,0 +1,10 @@
+public class HasTeen2 {
+  public boolean hasTeen(int a, int b, int c) {
+    if (a >= 13 && a <= 19) {
+      return true;
+    } else if (b >= 13 && b <= 19) {
+      return true;
+    } else
+      return (c >= 13 && c <= 19);
+  }
+}
